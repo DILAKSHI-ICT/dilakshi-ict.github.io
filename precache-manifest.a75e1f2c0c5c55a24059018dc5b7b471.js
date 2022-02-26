7094fa29d3a2cd4c8b0de7bb1135bca9b9920074
DILAKSHI-ICT <96301@fhss.sjp.ac.lk>
2022-02-27 02:06:32 +0530

    Updates

---
+++ precache-manifest.a75e1f2c0c5c55a24059018dc5b7b471.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1ad7df1f4f6df5a7300fd63872bc45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ee1b33bb0deb9ec2e7f",
+    "url": "/static/css/main.022eb82c.chunk.css"
+  },
+  {
+    "revision": "9cd56c8d50228cda48de",
+    "url": "/static/js/2.0b32ae95.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.0b32ae95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ee1b33bb0deb9ec2e7f",
+    "url": "/static/js/main.add3eead.chunk.js"
+  },
+  {
+    "revision": "95a2f2f62ce7543f12e4",
+    "url": "/static/js/runtime-main.10434ef0.js"
+  },
+  {
+    "revision": "47e8b0c148b80eb3a6b34e05bc80f842",
+    "url": "/static/media/Amour.47e8b0c1.jpg"
+  },
+  {
+    "revision": "350be7ca6a94521ab2776b1cf601e017",
+    "url": "/static/media/CV.350be7ca.pdf"
+  },
+  {
+    "revision": "00e67c03cf862699b78bd2cf273e0429",
+    "url": "/static/media/Profile.00e67c03.jpeg"
+  },
+  {
+    "revision": "279fbf3fa935c0eb34ea0897101455cf",
+    "url": "/static/media/image-0-02-06-7eaec8b33414fc993e2f3d3d6e7f8cec2d97d9879cd186d401f16732064ff583-V.279fbf3f.jpg"
+  },
+  {
+    "revision": "440a37560309b7020f5e0a19ae1b29e4",
+    "url": "/static/media/new.440a3756.jpg"
+  },
+  {
+    "revision": "c8d64e9daedd2fbafa932440772ccc88",
+    "url": "/static/media/pb.c8d64e9d.jpg"
+  }
+]);
